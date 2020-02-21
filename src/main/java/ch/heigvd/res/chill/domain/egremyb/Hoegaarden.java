2374af702873975b3c6715e1a2f2c2e3757298af
bruno <egremy.bruno@gmail.com>
2020-02-21 18:58:41 +0100

    Added the possibility to order Hoegaarden beers

---
+++ src/main/java/ch/heigvd/res/chill/domain/egremyb/Hoegaarden.java
@@ -0,0 +1,22 @@
+package ch.heigvd.res.chill.domain.egremyb;
+
+import ch.heigvd.res.chill.domain.IProduct;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+public class Hoegaarden implements IProduct {
+    public final static String NAME = "Hoegaarden";
+    public final static BigDecimal PRICE = new BigDecimal(3.5);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+}
