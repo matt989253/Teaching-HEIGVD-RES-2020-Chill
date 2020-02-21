13e8a71b0fe24418859a947692fea77c6bce4f2d
Sachouille <sacha.perdrizat@gmail.com>
2020-02-21 14:34:17 +0100

    add BFM torpille

--- src/main/java/ch/heigvd/res/chill/domain/sinyks/Torpille.java
+++ src/main/java/ch/heigvd/res/chill/domain/sinyks/Torpille.java
@@ -4,8 +4,8 @@ import ch.heigvd.res.chill.domain.IProduct;
 
 import java.math.BigDecimal;
 
-public abstract class BFM implements IProduct{
-    public final static String NAME = "BFM";
+public class Torpille implements IProduct{
+    public final static String NAME = "BFM - Torpille";
     public final static BigDecimal PRICE = new BigDecimal(3.0);
 
     @Override
