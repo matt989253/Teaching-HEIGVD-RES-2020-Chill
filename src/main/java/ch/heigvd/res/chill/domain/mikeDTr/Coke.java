cc6a8897e65c28dfa60aea421f547a684cd9c069
mikeDTr <michael.triponez@heig-vd.ch>
2020-02-21 14:18:41 +0100

    minor changes

--- src/main/java/ch/heigvd/res/chill/domain/mikeDTr/Coke.java
+++ src/main/java/ch/heigvd/res/chill/domain/mikeDTr/Coke.java
@@ -1,4 +1,21 @@
 package ch.heigvd.res.chill.domain.mikeDTr;
 
-public class Coke {
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Coke  implements IProduct {
+    public final static String NAME = "Coke";
+    public final static BigDecimal PRICE = new BigDecimal(2.5);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+
 }
--- src/test/java/ch/heigvd/res/chill/domain/mikeDTr/CokeTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/mikeDTr/CokeTest.java
@@ -1,5 +1,29 @@
+package ch.heigvd.res.chill.domain.mikeDTr;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CokeTest {
+    @Test
+    void thePriceAndNameForCokeShouldBeCorrect() {
+        Coke soft = new Coke();
+        assertEquals(soft.getName(), Coke.NAME);
+        assertEquals(soft.getPrice(), Coke.PRICE);
+    }
 
+    @Test
+    void aBartenderShouldAcceptAnOrderForCoke() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.wasadigi.Coke";
+        OrderRequest request = new OrderRequest(6, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Coke.PRICE.multiply(new BigDecimal(6));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
 }
