3c38195b5208cf077391f8c9debb6cc485f6d3b9
doeni98 <denis_bourqui@hotmail.com>
2020-02-23 12:56:48 +0100

    added chopfab

---
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/Chopfabtest.java
@@ -0,0 +1,29 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.wasadigi.Chopfab;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Chopfabtest {
+    @Test
+    void thePriceAndNameForBoxerShouldBeCorrect() {
+        Chopfab beer = new Chopfab();
+        assertEquals(beer.getName(), Chopfab.NAME);
+        assertEquals(beer.getPrice(), Chopfab.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForChopfab() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.doeni98.Chopfab";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Chopfab.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+}
